892b1e65cbe4f027dcabd7a16682b8351af91c6b
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-09 17:27:05 +0300

    added implementation of MyLinkedListNode

---
+++ GenericCollection.Presentation/GenericCollection.Collections/MyLinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericCollection.Collections
+{
+    public class MyLinkedListNode<T>
+    {
+        public MyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public MyLinkedListNode<T> Previous { get; set; } = null!;
+        public MyLinkedListNode<T> Next { get; set; } = null!;
+    }
+}

00ef9f1b50880958792f6a89fe1198fc2ea800e9
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-10 17:12:12 +0300

    Added base template for Invoker service

---
+++ GenericCollection/GenericCollection.BLL/Interfaces/IInvoker.cs
@@ -0,0 +1,12 @@
+﻿using GenericCollection.Collections;
+using System.Windows.Input;
+
+namespace GenericCollection.BLL.Interfaces
+{
+    public interface IInvoker
+    {
+        public void AddCommand(ICommand command);
+        public void ExecuteCommand(int index);
+        public MyLinkedList<ICommand> GetCommands();
+    }
+}
---
+++ GenericCollection/GenericCollection.BLL/Services/Invoker.cs
@@ -0,0 +1,19 @@
+﻿using GenericCollection.BLL.Interfaces;
+using GenericCollection.Collections;
+using System.Windows.Input;
+
+namespace GenericCollection.BLL.Services
+{
+    public class Invoker : IInvoker
+    {
+        private MyLinkedList<ICommand> commands = new MyLinkedList<ICommand>();
+
+        public MyLinkedList<ICommand> GetCommands() => commands;
+
+        public void AddCommand(ICommand command) => commands.Add(command);
+
+        public void ExecuteCommand(int index)
+        {
+        }
+    }
+}

45c42d870e9b0d79520f7afebe223e62bb4149d4
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-10 17:05:54 +0300

    added template for command pattern

---
+++ GenericCollection/GenericCollection.BLL/Helpers/CommandMenu.cs
@@ -0,0 +1,30 @@
+﻿using GenericCollection.BLL.Interfaces;
+using GenericCollection.BLL.Interfaces.Abstract;
+using GenericCollection.Collections;
+
+namespace GenericCollection.BLL.Helpers
+{
+    public static class CommandMenu
+    {
+        private const string NO_COMMANDS_ERROR_MESSAGE = "There are no commands in menu";
+
+        private static MyLinkedList<BaseCommand>? _commands;
+
+        public static void GenerateMenu(IEnumerable<BaseCommand> commands)
+        {
+            _commands = new MyLinkedList<BaseCommand>(commands);
+        }
+
+        public static void PrintMenu(IWriter writer)
+        {
+            if(_commands is not null) 
+            {
+                writer.WriteMyLinkedList(_commands);
+            }
+            else
+            {
+                writer.Write(ConsoleColor.Red, NO_COMMANDS_ERROR_MESSAGE);
+            }
+        }
+    }
+}
---
+++ GenericCollection/GenericCollection.BLL/Interfaces/Abstract/BaseCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericCollection.BLL.Interfaces.Abstract
+{
+    public abstract class BaseCommand
+    {
+        public abstract string Name { get; }
+
+        public abstract void Execute();
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}

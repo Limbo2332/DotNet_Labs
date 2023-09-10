7e337be9bf584de4ef9a7535b99ec321adcbff97
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-10 19:31:24 +0300

    added repository pattern

---
+++ GenericCollection/GenericCollection.DAL/Repositories/Interfaces/IMyLinkedListRepository.cs
@@ -0,0 +1,15 @@
+﻿using GenericCollection.Collections;
+
+namespace GenericCollection.DAL.Repositories.Interfaces
+{
+    public interface IMyLinkedListRepository<T>
+    {
+        MyLinkedList<T> GetCollection();
+        MyLinkedListNode<T>? GetItemByValue(T value);
+        MyLinkedListNode<T>? GetItemByIndex(int index);
+        void Add(T item);
+        void Add(MyLinkedListNode<T> node);
+        void Remove(T item);
+        void Remove(MyLinkedListNode<T> node);
+    }
+}
---
+++ GenericCollection/GenericCollection.DAL/Repositories/MyLinkedListRepository.cs
@@ -0,0 +1,44 @@
+﻿using GenericCollection.Collections;
+using GenericCollection.DAL.Repositories.Interfaces;
+
+namespace GenericCollection.DAL.Repositories
+{
+    public class MyLinkedListRepository<T> : IMyLinkedListRepository<T>
+    {
+        private MyLinkedList<T> _items = new MyLinkedList<T>();
+
+        public MyLinkedList<T> GetCollection()
+        {
+            return _items;
+        }
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void Add(MyLinkedListNode<T> node)
+        {
+            _items.Add(node);
+        }
+
+        public MyLinkedListNode<T>? GetItemByIndex(int index)
+        {
+            return _items[index];
+        }
+
+        public MyLinkedListNode<T>? GetItemByValue(T value)
+        {
+            return _items.Find(value);
+        }
+
+        public void Remove(T item)
+        {
+            _items.Remove(item);
+        }
+
+        public void Remove(MyLinkedListNode<T> node)
+        {
+            _items.Remove(node);
+        }
+    }
+}

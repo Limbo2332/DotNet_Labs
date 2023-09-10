7ff3d9cc5a9ff2ae2a9cf2376d1c2f70d1947b22
Limbo2332 <vorobiovalosha@gmail.com>
2023-09-10 16:46:51 +0300

    added check input data service

---
+++ GenericCollection/GenericCollection.BLL/Interfaces/ICheckData.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericCollection.BLL.Interfaces
+{
+    public interface ICheckData
+    {
+        T CheckData<T>(string inputTextGreeting, string errorMessage) where T : IParsable<T>;
+
+        string CheckStringData(string inputTextGreeting, string errorMessage);
+    }
+}
---
+++ GenericCollection/GenericCollection.BLL/Services/CheckData.cs
@@ -0,0 +1,62 @@
+﻿using GenericCollection.BLL.Interfaces;
+using System.Globalization;
+
+namespace GenericCollection.BLL.Services
+{
+    public class CheckData : ICheckData
+    {
+        private readonly IWriter _writer;
+
+        public CheckData(IWriter writer)
+        {
+            _writer = writer;
+        }
+
+        /// <summary>
+        /// Generic method to check input data, input data is mandatory
+        /// </summary>
+        /// <typeparam name="T">Type of input data</typeparam>
+        /// <param name="inputTextGreeting">Message to help entering value</param>
+        /// <param name="errorMessage">Error message if something goes wrong</param>
+        /// <returns>Valid value</returns>
+        T ICheckData.CheckData<T>(string inputTextGreeting, string errorMessage)
+        {
+            _writer.Write(ConsoleColor.Cyan, inputTextGreeting + "\n");
+            string? inputText = Console.ReadLine();
+            _writer.Write();
+
+            T? type;
+
+            while (!T.TryParse(inputText, CultureInfo.CurrentCulture, out type))
+            {
+                _writer.Write(ConsoleColor.Red, errorMessage + "\n");
+                inputText = Console.ReadLine();
+                _writer.Write();
+            }
+
+            return type;
+        }
+
+        /// <summary>
+        /// Method to check string input data, input data is mandatory
+        /// </summary>
+        /// <param name="inputTextGreeting">Message to help entering value</param>
+        /// <param name="errorMessage">Error message if something goes wrong</param>
+        /// <returns>Valid string</returns>
+        public string CheckStringData(string inputTextGreeting, string errorMessage)
+        {
+            _writer.Write(ConsoleColor.Green, inputTextGreeting + "\n");
+            string? inputText = Console.ReadLine();
+            _writer.Write();
+
+            while (string.IsNullOrEmpty(inputText))
+            {
+                _writer.Write(ConsoleColor.Red, errorMessage + "\n");
+                inputText = Console.ReadLine();
+                _writer.Write();
+            }
+
+            return inputText;
+        }
+    }
+}
